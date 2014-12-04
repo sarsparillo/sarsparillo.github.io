66d98f938337646eeee009e049a516eb20f2168c
sarsparillo <pacwang@gmail.com>
2014-12-03 23:24:30 -0500

    december_bancrf

--- js/controllers.js
+++ js/controllers.js
@@ -1,22 +1,180 @@
 var app = angular.module('kylePortfolio', []);
 
-app.controller('visualSchool', function()
-{
-	this.instances = pieces;
-
-	var pieces = [
-	{
-		imageUrl: '1/thumb.jpg',
-		title: 'monster truck',
-	},
-	{
-		imageUrl: '1/thumb.jpg',
-		title: 'bear house',
-	},
-	{
-		imageUrl: '1/thumb.jpg',
-		title: 'afraid of goats',
-	},
-
-	];
+app.run(function($rootScope) {
+	$rootScope.isHidden = true;
+	$rootScope.imageNumber = 0;
 });
+
+app.factory('detailUrl', function($rootScope) {
+	var currentUrl = {};
+	currentUrl.url = '';
+
+	currentUrl.changeUrl = function(newUrl) {
+		this.url = newUrl;
+		this.broadcastUrl();
+	};
+
+	currentUrl.broadcastUrl = function() { $rootScope.$broadcast('urlChange'); }
+	
+	return currentUrl;
+});
+
+app.directive('content', function() {
+	return {
+		restrict: 'E',
+		templateUrl: 'templates/thumb.html',
+		replace: true
+	};
+});
+
+app.directive('detail', function() {
+	return {
+		restrict: 'E',
+		templateUrl: 'templates/detail.html',
+		replace: true
+	};
+});
+
+app.directive('slider', function($rootScope) { //to reuse, replace "#gallery_img" with whatever you want
+	return {
+		restrict: 'E',
+		link: function(scope, element, attrs){
+			
+			var gallery = angular.element('#gallery_img');
+			element.bind('click', function(){
+
+				var totalWidth = gallery.children().eq($rootScope.imageNumber).width();
+				var last = gallery.children().eq((gallery.children().length) - 1);
+				
+				if (($(this).is("#next")) &&
+				((last.offset().left + last.width()) > angular.element('#next').offset().left)) 
+				{	
+					gallery.css({left: '-=' + totalWidth});
+					$rootScope.imageNumber++;
+				}
+				else if (($(this).is("#prev")) && ($rootScope.imageNumber > 0)) 
+				{	
+					$rootScope.imageNumber--
+					gallery.css({left: '+=' + totalWidth});
+				}
+			});
+		}
+	};
+});
+
+app.controller('navController', ['$scope', '$rootScope', 
+	function ($scope, $rootScope) {
+	$scope.hide = function() { 
+		$rootScope.isHidden = true; 
+		angular.element('#gallery_img').css({left: '100px'});
+		$rootScope.imageNumber = 0;
+	}
+}]);
+
+
+app.controller('detailController', ['$scope', '$http', '$rootScope', 'detailUrl', 
+	function ($scope, $http, $rootScope, detailUrl) {
+	$scope.$on('urlChange', function() {
+
+		var url = detailUrl.url;
+
+		$http.get(url + 'title.json').then(function(result){
+			$scope.header = result.data;
+		});
+
+		$http.get(url + 'thumbs.json').then(function(result){
+			$scope.thumbnails = result.data;
+		});
+	});
+}]);
+
+app.controller('visualDesign', ['$scope', '$http', '$rootScope', 'detailUrl',
+	function ($scope, $http, $rootScope, detailUrl) {
+
+	var url = 'js/json/design.json';
+	
+	$http.get(url).then(function(result){
+		$scope.instances = result.data;
+	});
+
+	$scope.updateUrl = function(newUrl) {
+		detailUrl.changeUrl(newUrl)
+		$rootScope.isHidden = false;
+	};
+}]);
+
+app.controller('visualArt', ['$scope', '$http', '$rootScope', 'detailUrl',
+	function ($scope, $http, $rootScope, detailUrl) {
+	"use strict";
+
+	$scope.url = 'js/json/art.json';
+
+	$http.get('js/json/art.json').then(function(result){
+		$scope.instances = result.data;
+	});
+
+	$scope.updateUrl = function(newUrl) {
+		detailUrl.changeUrl(newUrl)
+		$rootScope.isHidden = false;
+	};
+}]);
+
+app.controller('visualArchitecture', ['$scope', '$http', '$rootScope', 'detailUrl',
+	function ($scope, $http, $rootScope, detailUrl) {
+
+	var url = 'js/json/architecture.json';
+	
+	$http.get(url).then(function(result){
+		$scope.instances = result.data;
+	});
+
+	$scope.updateUrl = function(newUrl) {
+		detailUrl.changeUrl(newUrl)
+		$rootScope.isHidden = false;
+	};
+}]);
+
+app.controller('musicSolo', ['$scope', '$http', '$rootScope', 'detailUrl',
+	function ($scope, $http, $rootScope, detailUrl) {
+
+	var url = 'js/json/solo.json';
+
+	$http.get('js/json/solo.json').then(function(result){
+		$scope.instances = result.data;
+	});
+
+	$scope.updateUrl = function(newUrl) {
+		detailUrl.changeUrl(newUrl)
+		$rootScope.isHidden = false;
+	};
+}]);
+
+app.controller('musicVideos', ['$scope', '$http', '$rootScope', 'detailUrl',
+	function ($scope, $http, $rootScope, detailUrl) {
+	
+	var url = 'js/json/videos.json';
+
+	$http.get('js/json/videos.json').then(function(result){
+		$scope.instances = result.data;
+	});
+
+	$scope.updateUrl = function(newUrl) {
+		detailUrl.changeUrl(newUrl)
+		$rootScope.isHidden = false;
+	};
+}]);
+
+app.controller('musicCollaborations', ['$scope', '$http', '$rootScope', 'detailUrl',
+	function ($scope, $http, $rootScope, detailUrl) {
+	
+	var url = 'js/json/collaborations.json';
+
+	$http.get('js/json/collaborations.json').then(function(result){
+		$scope.instances = result.data;
+	});
+
+	$scope.updateUrl = function(newUrl) {
+		detailUrl.changeUrl(newUrl)
+		$rootScope.isHidden = false;
+	};
+}]);
--- js/scroll.js
+++ js/scroll.js
@@ -1,16 +1,125 @@
 jQuery(function($)
-{	
-	// nav buttons	
-	$("#about_nav").bind('click', function() {
-		$("section").toggleClass("main about");
+{
+	// generic variables
+	var splash = $('#splash'),
+		header = $('header'),
+		visual_splash = $('#visual_splash'),
+		visual_header = $('#visual_header'),
+		visual_text = $('#visual_text'),
+		music_splash = $('#music_splash'),
+		music_header = $('#music_header'),
+		music_text = $('#music_text');
+
+	// check what browser is in use to fix scrolling 
+	var body = "";
+	function theBrowser() {
+		if ((navigator.userAgent.search("Chrome") >= 0) 
+			|| (navigator.userAgent.search("Safari") >= 0))
+		{ body = $('body'); }
+		else 
+		{ body = $('html'); }
+	};
+	theBrowser();
+
+	// header position initialization
+	function initialize(headerName, splashName)
+	{
+		headerName.css('top', (Math.round(body.scrollTop()-splashName.offset().top)
+			+ splashName.outerHeight()) + 'px')
+	};
+	initialize(visual_header, visual_splash);
+	initialize(music_header, music_splash);
+
+	// navigation
+	var move = {
+			to_top: function(e)
+			{ scrollTo($('body')); },
+			to_visual: function(e)
+			{ scrollTo(visual_splash); },
+			to_music: function(e)
+			{ scrollTo(music_splash); },
+			show: function(e)
+			{ $('body').css({'overflow': 'hidden'}); },
+			hide: function(e)
+			{ $('body').css({'overflow': 'scroll'}); },
+		};
+
+	function scrollTo(location)
+	{
+		$('html, body').stop().animate(
+			{ scrollTop: location.offset().top }, 500);
+	};
+
+	// navigation handler
+	$('a[data-action]').on('click', function(e)
+	{
+		e.preventDefault(); // remove hash from url
+		var action = $(this).data("action");
+
+		// if an action has this name, do it
+		if (typeof move[action] === "function")
+		{ move[action].call(this, e); }
 	});
 
-	$("#company_nav").bind('click', function() {
-		$("section").toggleClass("main company");
-	});
+	// header scrolling
+	function headScroll() 
+	{
+		var amount = ($(window).height() - 618) - Math.round(body.scrollTop());
+		splash.css('top', amount+'px');
+		header.css('top', (splash.innerHeight()+amount)+'px');
+	};
 
-	$("#contact_nav").bind('click', function() {
-		$("section").toggleClass("main contact");
+	// scroll handler
+	$(window).bind('scroll resize', function() 
+	{
+		// functions y'all
+		function offsetAmount(div_name) 
+		{ return 0-Math.round(body.scrollTop() - div_name.offset().top) };
+
+		function divBase(div_name) 
+		{ return offsetAmount(div_name) + div_name.outerHeight(); };
+
+		function scrollThis(div_target, div_offset)
+		{ div_target.css('top', divBase(div_offset) + 'px') };
+
+		function showHide(div_target, div_offset, x){
+			if (offsetAmount(div_offset) < x)
+			{ div_target.removeClass('big').addClass('small'); }
+			else
+			{ div_target.removeClass('small').addClass('big'); };
+		};
+
+		// header resizing
+		showHide($(".heads"), visual_splash, 520);
+		showHide(visual_text, visual_splash, 0);
+		showHide(music_text, music_splash, 0);
+
+		// keep the detail box in screen no matter what
+		$('#detail_box').css('top', divBase(header) + 'px');
+
+		// more complex header junk
+		if ((offsetAmount(visual_splash) < 618)
+			&& (offsetAmount(visual_splash) > 60))
+		{ headScroll(); }
+		else if (offsetAmount(visual_splash) < 60)
+		{
+			splash.css('top', -512);
+			header.css('top', 0);
+		}
+		else if (offsetAmount(visual_splash) > 618)
+		{
+			splash.css('top', 0);
+			header.css('top', Math.round(splash.outerHeight())+'px');
+		};
+
+		if (divBase(visual_splash) > 60)
+		{ scrollThis(visual_header, visual_splash); }
+		else
+		{ visual_header.css('top', '60px'); };
+
+		if (divBase(music_splash) > 111)
+		{ scrollThis(music_header, music_splash); }
+		else
+		{ music_header.css('top', '111px'); };
 	});
-	
 }); 
