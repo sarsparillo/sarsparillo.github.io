4a5aa8ead545aa9a041356dd32f7f2609fc55fc9
Tom Whalley <whalleywrites@gmail.com>
2017-05-05 07:55:12 -0400

    Added minimize header on scroll

--- js/main.js
+++ js/main.js
@@ -1,9 +1,4 @@
 $(function() {
-	
-$('#test').click(function(){
-	$('header').toggleClass('banner');
-	
-});
 
 $('.website-text').hover(function(){
 	var wabsit = $(this);
@@ -12,23 +7,14 @@ $('.website-text').hover(function(){
 	var wabsit = $(this);
 	wabsit.text(wabsit.data('original'))
 });
-/*
-var header = $('header');
-var headPosition = header.offset().top;
 
 $(window).scroll($.throttle(10, function() {
-
-	header.css("top", 0);
-	var edge = $(window).scrollTop() + 100;
-
-	if (headPosition <= edge) {
-		header.addClass("banner");
-	}
-	else {
-		header.removeClass("banner");
-	}
-
-
+	var header = $('header');
+  if ($(document).scrollTop() > 50) {
+    header.addClass('banner');
+  } else {
+    header.removeClass('banner');
+  }
 }));
-	*/
+
 });
