6ccb7e86aa44c2335b262a97af9c886516eaa7f5
Tom Whalley <sarsparillo@users.noreply.github.com>
2022-06-20 12:07:40 -0400

    fixing tabs

--- js/main.js
+++ js/main.js
@@ -4,36 +4,34 @@ const $sections = $('section');
 let currentPage;
 
 $navlinks.on({
-  mouseenter: function () {
-    const $this = $(this);
-    if (!$this.hasClass('active')) {
-      $this.addClass('active');
-    }
-  },
-  mouseleave: function () {
-    const $this = $(this);
-    if ($this.attr('id') !== currentPage) {
-      $this.removeClass('active'); 
-    }
-  }
+	mouseenter: function () {
+		const $this = $(this);
+		if (!$this.hasClass('active')) {
+			$this.addClass('active');
+		}
+	},
+	mouseleave: function () {
+		const $this = $(this);
+		if ($this.attr('id') !== currentPage) {
+			$this.removeClass('active'); 
+		}
+	}
 });
 
 $navlinks.click(function(){
-  const $this = $(this);
-  if ($header.hasClass('banner') && $this.attr('id') == currentPage) {
-    $header.removeClass('banner');
-    $navlinks.removeClass('active');
-    $sections.removeClass('active');
-    currentPage = '';
-  } else {
-    $header.addClass('banner');    
-    $navlinks.removeClass('active');
-    $sections.removeClass('active');
-    $this.addClass('active');
-    currentPage = $this.attr('id');
-    const currentSection = '#content-' + currentPage;
-    $(currentSection).addClass('active');
-  }
+	const $this = $(this);
+	if ($header.hasClass('banner') && $this.attr('id') == currentPage) {
+		$header.removeClass('banner');
+		$navlinks.removeClass('active');
+		$sections.removeClass('active');
+		currentPage = '';
+	} else {
+		$header.addClass('banner');    
+		$navlinks.removeClass('active');
+		$sections.removeClass('active');
+		$this.addClass('active');
+		currentPage = $this.attr('id');
+		const currentSection = '#content-' + currentPage;
+		$(currentSection).addClass('active');
+	}
 });
-
-$('#content-about').load('/fragments/about.html');
