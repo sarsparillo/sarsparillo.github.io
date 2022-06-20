e08f382134efce249d238bb7526d946e138e7b24
Tom Whalley <sarsparillo@users.noreply.github.com>
2022-06-20 12:32:28 -0400

    basic contact form, basic details

--- js/main.js
+++ js/main.js
@@ -35,3 +35,34 @@ $navlinks.click(function(){
 		$(currentSection).addClass('active');
 	}
 });
+
+var form = document.getElementById("email-form");
+			
+async function handleSubmit(event) {
+	event.preventDefault();
+	var status = document.getElementById("form-status");
+	var data = new FormData(event.target);
+	fetch(event.target.action, {
+	method: form.method,
+	body: data,
+	headers: {
+		'Accept': 'application/json'
+	}
+	}).then(response => {
+	if (response.ok) {
+		status.innerHTML = "Thanks for contacting me! Either I'll reply, or I won't - there's no guarantee what'll happen in the future!";
+		form.reset()
+	} else {
+		response.json().then(data => {
+		if (Object.hasOwn(data, 'errors')) {
+			status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+		} else {
+			status.innerHTML = "Oops! There was a problem sending your message. Try again in a bit, or contact via other methods."
+		}
+		})
+	}
+	}).catch(error => {
+	status.innerHTML = "Oops! There was a problem sending your message. Try again in a bit, or contact via other methods."
+	});
+}
+form.addEventListener("submit", handleSubmit);
