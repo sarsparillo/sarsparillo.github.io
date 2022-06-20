84b349e9aa741f2e3d5eb4b3102814f090257a36
Tom Whalley <sarsparillo@users.noreply.github.com>
2022-06-20 11:59:26 -0400

    updated css, included terrazzo img, renamed to writer deets

--- js/main.js
+++ js/main.js
@@ -1,20 +1,39 @@
-$(function() {
+const $header = $('header');
+const $navlinks = $('nav li');
+const $sections = $('section');
+let currentPage;
 
-$('.website-text').hover(function(){
-	var wabsit = $(this);
-	wabsit.text("website");
-}, function() {
-	var wabsit = $(this);
-	wabsit.text(wabsit.data('original'))
-});
-
-$(window).scroll($.throttle(10, function() {
-	var header = $('header');
-  if ($(document).scrollTop() > 50) {
-    header.addClass('banner');
-  } else {
-    header.removeClass('banner');
+$navlinks.on({
+  mouseenter: function () {
+    const $this = $(this);
+    if (!$this.hasClass('active')) {
+      $this.addClass('active');
+    }
+  },
+  mouseleave: function () {
+    const $this = $(this);
+    if ($this.attr('id') !== currentPage) {
+      $this.removeClass('active'); 
+    }
   }
-}));
-
 });
+
+$navlinks.click(function(){
+  const $this = $(this);
+  if ($header.hasClass('banner') && $this.attr('id') == currentPage) {
+    $header.removeClass('banner');
+    $navlinks.removeClass('active');
+    $sections.removeClass('active');
+    currentPage = '';
+  } else {
+    $header.addClass('banner');    
+    $navlinks.removeClass('active');
+    $sections.removeClass('active');
+    $this.addClass('active');
+    currentPage = $this.attr('id');
+    const currentSection = '#content-' + currentPage;
+    $(currentSection).addClass('active');
+  }
+});
+
+$('#content-about').load('/fragments/about.html');
